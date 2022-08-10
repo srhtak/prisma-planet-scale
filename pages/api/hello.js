7fe1ac231a08753d545c40c93802384e409618aa
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-08-10 23:42:37 +0300

    prisma init && pscale promote

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    return await addBook(req, res);
+  } else if (req.method === "GET") {
+    return await readBooks(req, res);
+  } else {
+    return res
+      .status(405)
+      .json({ message: "Method not allowed", success: false });
+  }
+}
+
+async function addBook(req, res) {
+  const body = req.body;
+  try {
+    const newEntry = await prisma.bookSuggestions.create({
+      data: {
+        bookTitle: body.bookTitle,
+        bookAuthor: body.bookAuthor,
+        bookGenre: body.bookGenre,
+      },
+    });
+    return res.status(200).json(newEntry, { success: true });
+  } catch (error) {
+    console.error("Request error", error);
+    res.status(500).json({ error: "Error adding book", success: false });
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,102 @@
 import { useState } from "react";
 
 export default function Home() {
-  return <div className="text-red-500 bg-blue-400 p-5">Home</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+  return (
+    <section className="py-6 bg-coolGray-100 text-coolGray-900">
+      <div
+        className="
+      grid
+      max-w-6xl
+      grid-cols-1
+      px-6
+      mx-auto
+      lg:px-8
+      md:grid-cols-2 md:divide-x
+    "
+      >
+        <div className="py-6 md:py-0 md:px-6">
+          <h1 className="text-4xl font-bold">Books</h1>
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="
+        flex flex-col
+        py-6
+        space-y-6
+        md:py-0 md:px-6
+        ng-untouched ng-pristine ng-valid
+      "
+        >
+          <label className="block">
+            <span className="mb-1">Book Title</span>
+            <input
+              type="text"
+              className="
+              h-12
+            block
+            w-full
+            border
+            rounded-md
+            shadow-sm
+            focus:ring focus:ring-opacity-75
+            bg-coolGray-100
+          "
+            />
+          </label>
+          <label className="block">
+            <span className="mb-1">Author Name</span>
+            <input
+              type="email"
+              className="
+              h-12
+            block
+            border
+            w-full
+            rounded-md
+            shadow-sm
+            focus:ring focus:ring-opacity-75 
+            bg-coolGray-100
+          "
+            />
+          </label>
+          <label className="block">
+            <span className="mb-1">Genre</span>
+            <input
+              type="email"
+              className="
+              h-12
+            block
+            border
+            w-full
+            rounded-md
+            shadow-sm
+            focus:ring focus:ring-opacity-75 
+            bg-coolGray-100
+          "
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="
+          self-center
+          px-8
+          py-3
+          text-lg
+          rounded
+          text-white
+          bg-black
+          focus:bg-gray-700 focus:outline-none
+        "
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
